a7dda13dda8eaf369c0f5dc02a621379bcf10c12
root <kjpark4321@naver.com>
2022-04-28 05:06:09 +0000

    [feat #5] add metric roc_auc, accuracy

--- Development/pytorch-template/model/metric.py
+++ Development/pytorch-template/model/metric.py
@@ -1,13 +1,22 @@
 import torch
+import numpy as np
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 
-def accuracy(output, target):
+def mnist_accuracy(output, target):
     with torch.no_grad():
         pred = torch.argmax(output, dim=1)
         assert pred.shape[0] == len(target)
         correct = 0
         correct += torch.sum(pred == target).item()
     return correct / len(target)
+
+
+def accuracy(output, target):
+    # output = output.detach().cpu().numpy()
+    # target = target.detach().cpu().numpy()
+    acc = accuracy_score(target, np.where(output >= 0.5, 1, 0))
+    return acc
 
 
 def top_k_acc(output, target, k=3):
@@ -18,3 +27,8 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def roc_auc(output, target):
+    auc = roc_auc_score(target, output)
+    return auc
